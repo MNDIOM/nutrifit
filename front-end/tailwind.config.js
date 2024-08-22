0a27c11edb58d548619323c0690cdef8fec876f9
MNDIOM <mamadou.johm@gmail.com>
2024-08-22 01:26:36 -0400

    msg

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
